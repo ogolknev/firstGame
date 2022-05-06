f5608922f98769f4e2919f301833c581bc26b985
Ogolknev Nikita <ogolknev257@mail.ru>
2022-05-07 01:18:14 +0300

    add rand spawn of objects, behavior of entities and attack mechanics

---
+++ firstGame/containers.h
@@ -0,0 +1,327 @@
+/*
+ * containers.h
+ *
+ *  Created on: May 6, 2022
+ *      Author: Никита
+ */
+
+#ifndef CONTAINERS_H_
+#define CONTAINERS_H_
+
+struct object{
+	int HP;
+	int DMG;
+	int LVL;
+	int moving;
+	int movingchek;
+	_Bool playerchek;
+	SDL_Rect rectangle;
+	SDL_Texture *texture;
+};
+
+typedef struct element{
+	struct object data;
+    struct element *next, *previous, *head, *tail;
+}container;
+
+container *newcontainer(){
+    container *newcontainer = malloc(sizeof(container));
+    newcontainer->next = NULL;
+    newcontainer->head = NULL;
+    newcontainer->tail = NULL;
+    newcontainer->previous = NULL;
+    return newcontainer;
+}
+
+//checking if the container is empty
+int is_empty(container **headcontainer){
+    container *extelement = *headcontainer;
+    if(extelement->head == NULL){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+//counting container elements
+int element_count(container **headcontainer){
+    container *extelement = newcontainer();
+    extelement = *headcontainer;
+    int counter = 0;
+    if(is_empty(&extelement) == 0){
+        return 0;
+    }
+    else{
+        while (extelement != NULL){
+            counter++;
+            extelement = extelement->next;
+        }
+        return counter;
+    }
+}
+
+
+//stack
+
+//function to add a new element to the stack
+void addtostack(container **headstack, struct object data){
+    container *extelement = newcontainer();
+    container *addelement = newcontainer();
+    extelement = *headstack;
+    if(element_count(&extelement) == 0){
+        addelement->data = data;
+        addelement->head = addelement;
+        addelement->tail = addelement;
+        extelement = addelement;
+    }
+    else{
+        addelement->data = data;
+        addelement->head = addelement;
+        addelement->next = extelement;
+        addelement->tail = extelement->tail;
+        extelement->previous = addelement;
+        while (extelement != NULL){
+            extelement->head = addelement;
+            extelement = extelement->next;
+        }
+        extelement = addelement;
+    }
+    *headstack = extelement;
+}
+
+//function to pop an element from the stack
+struct object getstack(container **headstack){
+    container *extstack = *headstack;
+    struct object getvalue = extstack->data;
+    if (element_count(&extstack) == 1){
+        extstack->next = NULL;
+        extstack->head = NULL;
+        extstack->tail = NULL;
+        extstack->previous = NULL;
+    } else if (element_count(&extstack) > 1){
+        extstack = extstack->next;
+        extstack->head = extstack;
+        extstack->previous = NULL;
+        while (extstack->next != NULL){
+            extstack = extstack->next;
+            extstack->head = extstack->previous->head;
+        }
+        extstack = extstack->head;
+        *headstack = extstack;
+    }
+    return getvalue;
+}
+
+
+//queue
+
+//function to add a new element to the queue
+void addtoqueue(container **headqueue, struct object data){
+    container *extelement = newcontainer();
+    container *addelement = newcontainer();
+    extelement = *headqueue;
+    if(element_count(&extelement) == 0){
+        addelement->data = data;
+        addelement->head = addelement;
+        addelement->tail = addelement;
+        extelement = addelement;
+    }
+    else{
+        addelement->data = data;
+        addelement->head = addelement;
+        addelement->next = extelement;
+        addelement->tail = extelement->tail;
+        extelement->previous = addelement;
+        while (extelement != NULL){
+            extelement->head = addelement;
+            extelement = extelement->next;
+        }
+        extelement = addelement;
+    }
+    *headqueue = extelement;
+}
+
+//function to pop an element from the queue
+struct object getqueue(container **headqueue){
+    container *extqueue = *headqueue;
+    struct object getdata;
+    if (element_count(&extqueue) == 1){
+        getdata = extqueue->data;
+        extqueue->next = NULL;
+        extqueue->head = NULL;
+        extqueue->tail = NULL;
+        extqueue->previous = NULL;
+    } else if (element_count(&extqueue) > 1){
+        getdata = extqueue->tail->data;
+        extqueue = extqueue->tail->previous;
+        extqueue->tail = extqueue;
+        extqueue->next = NULL;
+        while (extqueue->previous != NULL){
+            extqueue = extqueue->previous;
+            extqueue->tail = extqueue->next->tail;
+        }
+        extqueue = extqueue->head;
+        *headqueue = extqueue;
+    }
+    return getdata;
+}
+
+
+//list
+
+//function to add a new element to the beginning of the list
+void addfirst(container **headlist, struct object data){
+    container *extelement = *headlist;
+    container *addelement = newcontainer();
+    addelement->data = data;
+    addelement->next = extelement;
+    addelement->head = addelement;
+    extelement->previous = addelement;
+    while (extelement != NULL){
+        extelement->head = addelement;
+        extelement = extelement->next;
+    }
+    *headlist = addelement;
+}
+
+//function to add a new element to the end of the list
+void addlast(container **headlist, struct object data){
+    container *extelement = *headlist;
+    container *addelement = newcontainer();
+    addelement->data = data;
+    addelement->head = extelement->head;
+    while(extelement->next != NULL){
+        extelement = extelement->next;
+    }
+    extelement->next = addelement;
+    addelement->previous = extelement;
+    extelement = extelement->head;
+    *headlist = extelement;
+}
+
+//function to add a new element after the specified number
+void addtolist(container **headlist, struct object data, int number){
+    container *extelement = newcontainer();
+    container *addelement = newcontainer();
+    extelement = *headlist;
+    if(element_count(&extelement) == 0){
+        addelement->data = data;
+        addelement->head = addelement;
+        extelement = addelement;
+    }
+    else if(number == -1){
+        addfirst(&extelement, data);
+    }
+    else if(number == element_count(&extelement) - 1){
+        addlast(&extelement, data);
+    }
+    else{
+        int counter = 0;
+        while(extelement->next != NULL){
+            if (counter == number){
+                addelement->data = data;
+                addelement->head = extelement->head;
+                addelement->previous = extelement;
+                addelement->next = extelement->next;
+                extelement->next->previous = addelement;
+                extelement->next = addelement;
+            }
+            counter++;
+            extelement = extelement->next;
+        }
+        extelement = extelement->head;
+    }
+    *headlist = extelement;
+}
+
+//function to remove a new element to the beginning of the list
+void delfirst(container **headlist){
+    container *extelement = *headlist;
+    extelement = extelement->next;
+    extelement->head = extelement;
+    extelement->previous = NULL;
+    while (extelement->next != NULL){
+        extelement = extelement->next;
+        extelement->head = extelement->previous->head;
+    }
+    extelement = extelement->head;
+    *headlist = extelement;
+}
+
+//function to remove a new element to the end of the list
+void dellast(container **headlist){
+    container *extelement = *headlist;
+    while (extelement->next != NULL) {
+        extelement = extelement->next;
+    }
+    extelement->previous->next = NULL;
+    extelement = extelement->head;
+    *headlist = extelement;
+}
+
+//function to remove the specified element
+void delelement(container **headlist, int number){
+    container *extelement = newcontainer();
+    extelement = *headlist;
+    int counter = 0;
+    if(number == 0){
+        delfirst(&extelement);
+    }
+    else if(number == element_count(&extelement) - 1){
+        dellast(&extelement);
+    }
+    else {
+        while (extelement->next != NULL) {
+            if (counter == number) {
+                extelement->previous->next = extelement->next;
+                extelement->next->previous = extelement->previous;
+            }
+            counter++;
+            extelement = extelement->next;
+        }
+    }
+    extelement = extelement->head;
+    *headlist = extelement;
+}
+
+//function to get the value of the specified element
+struct object getlistdata(container **headlist, int number){
+    container *pointer = *headlist;
+    container *getlist = newcontainer();
+    int counter = 0;
+    if(element_count(&pointer) - 1 != number) {
+        while (pointer != NULL) {
+            if (counter == number) {
+                getlist->data = pointer->data;
+            }
+            counter++;
+            pointer = pointer->next;
+        }
+    }
+    else if(element_count(&pointer) - 1 == number){
+        while (pointer->next != NULL) {
+            pointer = pointer->next;
+        }
+        getlist->data = pointer->data;
+    }
+    pointer = NULL;
+    return getlist->data;
+}
+
+//function to get the address of the specified element
+container *getlist(container **headlist, int number){
+    container *extlist = *headlist;
+    int counter = 0;
+    while (extlist != NULL){
+        if(number == counter){
+            return extlist;
+        }
+        counter++;
+        extlist = extlist->next;
+    }
+    return extlist;
+}
+
+
+#endif /* CONTAINERS_H_ */
---
+++ firstGame/main.c
@@ -0,0 +1,349 @@
+#include <SDL.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include "containers.h"
+#include <time.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 640;
+int run = 1;
+int attackrecharge = 0;
+int angle = 0;
+SDL_Window *mainWindow = NULL;
+SDL_Renderer *mainRender = NULL;
+SDL_Event *mainEvent;
+container* objects;
+
+int initSDL(){
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+		printf("init error: %s\n", SDL_GetError());
+		return 1;
+	}
+	else return 0;
+}
+
+int limitedrandom(int botEdge, int topEdge, int extra){
+	int ttime = time(NULL);
+	srand(ttime + extra);
+	return botEdge + (rand() % (topEdge - botEdge));
+}
+
+void randSpawn(SDL_Rect *objrect, SDL_Rect bgrect, int extra){
+	objrect->x = bgrect.x + limitedrandom(0, 20, extra*752) * 32;
+	objrect->y = bgrect.y + (limitedrandom(0, 20, extra * extra * 257257)) * 32;
+}
+
+void multiSpawn(SDL_Renderer *render, container **headcontainer, struct object object, SDL_Rect bgrect, int number){
+	SDL_Rect rectangle = object.rectangle;
+	container *extcontainer = *headcontainer;
+	for(int i = 0; i < number; i++){
+		randSpawn(&rectangle, bgrect, i + 1);
+		object.rectangle = rectangle;
+		addlast(&extcontainer, object);
+	}
+	extcontainer = *headcontainer;
+}
+
+int calculateEntityMoving(struct object entity, container **headcontainer, int randvar){
+	container *extcontainer = *headcontainer;
+	int moving = 0;
+	int counter = 0;
+	int unmoving[4] = {0, 0, 0, 0};
+	for(int i = 0; i < element_count(&extcontainer); i++){
+		if(abs(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x) <= 32 && abs(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y) <= 32){
+			if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == 32){
+				if(entity.rectangle.y == getlist(&extcontainer, i)->data.rectangle.y){
+					unmoving[3] = 4;
+				}
+			}
+			if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == -32){
+				if(entity.rectangle.y == getlist(&extcontainer, i)->data.rectangle.y){
+					unmoving[1] = 2;
+				}
+			}
+			if(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == 32){
+				if(entity.rectangle.x == getlist(&extcontainer, i)->data.rectangle.x){
+					unmoving[0] = 1;
+				}
+			}
+			if(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == -32){
+				if(entity.rectangle.x == getlist(&extcontainer, i)->data.rectangle.x){
+					unmoving[2] = 3;
+				}
+			}
+			if(getlist(&extcontainer, i)->data.moving != -1){
+				if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == -32
+				&& entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == -32){
+					unmoving[1] = 2;
+					unmoving[2] = 3;
+				}
+				if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == 32
+				&& entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == 32){
+					unmoving[0] = 1;
+					unmoving[3] = 4;
+				}
+				if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == 32
+				&& entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == -32){
+					unmoving[3] = 4;
+					unmoving[2] = 3;
+				}
+				if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == -32
+				&& entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == 32){
+					unmoving[1] = 1;
+					unmoving[2] = 2;
+			}
+			}
+		}
+	}
+	if(entity.moving == -1){
+		return -1;
+	}
+	else if((unmoving[0] == 1 && unmoving[1] == 2 && unmoving[2] == 3 && unmoving[3] == 4)){
+		return 0;
+	}
+	else{
+		for(int i = 0; i < 4; i++){
+			if(unmoving[i] == 0){
+				unmoving[0] = 0;
+				unmoving[counter + 1] = i + 1;
+				counter++;
+			}
+		}
+		moving = unmoving[limitedrandom(0,counter + 1, randvar)];
+		return moving;
+	}
+}
+
+int *calculateTrafficBans(struct object entity, container **headcontainer){
+	container *extcontainer = *headcontainer;
+	int *unmoving = malloc(4*sizeof(int));
+	for(int i = 0; i < element_count(&extcontainer); i++){
+		if(abs(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x) <= 32 && abs(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y) <= 32){
+			if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == 32){
+				if(entity.rectangle.y == getlist(&extcontainer, i)->data.rectangle.y){
+					unmoving[3] = 4;
+				}
+			}
+			if(entity.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x == -32){
+				if(entity.rectangle.y == getlist(&extcontainer, i)->data.rectangle.y){
+					unmoving[1] = 2;
+				}
+			}
+			if(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == 32){
+				if(entity.rectangle.x == getlist(&extcontainer, i)->data.rectangle.x){
+					unmoving[0] = 1;
+				}
+			}
+			if(entity.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y == -32){
+				if(entity.rectangle.x == getlist(&extcontainer, i)->data.rectangle.x){
+					unmoving[2] = 3;
+				}
+			}
+		}
+	}
+	return unmoving;
+}
+
+struct object entityMoving(struct object entity, container **headcontainer, int randvar){
+	container *extcontainer = *headcontainer;
+	if(entity.moving == 0 && entity.playerchek == 0){
+		entity.moving = calculateEntityMoving(entity, &extcontainer, randvar);
+	}
+	else if(entity.moving == 1){
+		entity.movingchek++;
+		entity.rectangle.y--;
+	}
+	else if(entity.moving == 2){
+		entity.movingchek++;
+		entity.rectangle.x++;
+	}
+	else if(entity.moving == 3){
+		entity.movingchek++;
+		entity.rectangle.y++;
+	}
+	else if(entity.moving == 4){
+		entity.movingchek++;
+		entity.rectangle.x--;
+	}
+	if(entity.movingchek == 32){
+		entity.movingchek = 0;
+		entity.moving = 0;
+	}
+	return entity;
+}
+
+struct object playerMoving(struct object prect, int *trafficbans, const Uint8 *keyboardState){
+	if((keyboardState[SDL_SCANCODE_UP])||(keyboardState[SDL_SCANCODE_W]))
+		if(prect.moving == 0 && trafficbans[0] == 0) prect.moving = 1;
+
+	if((keyboardState[SDL_SCANCODE_DOWN])||(keyboardState[SDL_SCANCODE_S]))
+		if(prect.moving == 0 && trafficbans[2] == 0) prect.moving = 3;
+
+	if((keyboardState[SDL_SCANCODE_LEFT])||(keyboardState[SDL_SCANCODE_A]))
+		if(prect.moving == 0 && trafficbans[3] == 0) prect.moving = 4;
+
+	if((keyboardState[SDL_SCANCODE_RIGHT])||(keyboardState[SDL_SCANCODE_D]))
+		if(prect.moving == 0 && trafficbans[1] == 0) prect.moving = 2;
+	return prect;
+}
+
+container *attack(struct object attacker, container **headcontainer){
+	container *extcontainer = *headcontainer;
+	int deathcounter = 0;
+	int counter = 0;
+	for(int i = 0; i < element_count(&extcontainer); i++){
+		if(abs(attacker.rectangle.x - getlist(&extcontainer, i)->data.rectangle.x) <= 32
+		&& abs(attacker.rectangle.y - getlist(&extcontainer, i)->data.rectangle.y) <= 32
+		&& getlist(&extcontainer, i)->data.playerchek != 1){
+			getlist(&extcontainer, i)->data.HP--;
+			if(getlist(&extcontainer, i)->data.HP == 0){
+				deathcounter++;
+			}
+		}
+		while(deathcounter != 0){
+			if(getlist(&extcontainer, counter)->data.HP == 0){
+				delelement(&extcontainer, counter);
+				deathcounter--;
+			}
+			else{
+				counter++;
+			}
+		}
+	}
+	return *headcontainer;
+}
+
+int main(int argc, char* argv[]){
+	if(initSDL() == 1){
+		return 0;
+	}
+	mainWindow = SDL_CreateWindow("firstGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if(mainWindow == NULL){
+		printf("SDL could not create window! SDL error: %s\n", SDL_GetError());
+		return 1;
+	}
+	mainRender = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if(mainRender == NULL){
+		printf("SDL could not create render! SDL error: %s\n", SDL_GetError());
+		return 1;
+	}
+	mainEvent = malloc(sizeof(SDL_Event));
+
+	SDL_Texture *bgTexture = IMG_LoadTexture(mainRender,"Textures/backgrounds/background_0.png");
+	SDL_Rect bgrect;
+	bgrect.h = 640;
+	bgrect.w = 640;
+	bgrect.x = (SCREEN_WIDTH - bgrect.w) / 2;
+	bgrect.y = (SCREEN_HEIGHT - bgrect.h) / 2;
+
+	SDL_Texture *pTexture = IMG_LoadTexture(mainRender,"Textures/helmet20.png");
+	SDL_Rect prect;
+	prect.h = 41;
+	prect.w = 40;
+	prect.x = bgrect.x + (bgrect.w - prect.w) / 2 - 16;
+	prect.y = bgrect.y + (bgrect.h - prect.h) / 2 - 16;
+
+	SDL_Texture *sTexture = IMG_LoadTexture(mainRender, "Textures/sword.png");
+	SDL_Rect srect;
+	srect.h = 55;
+	srect.w = 32;
+	SDL_Point swordCenter;
+	swordCenter.x = srect.w / 2;
+	swordCenter.y = 0;
+
+	SDL_Texture *stTexture = IMG_LoadTexture(mainRender,"Textures/stan.png");
+	SDL_Rect strect;
+	strect.h = 32;
+	strect.w = 32;
+
+	SDL_Texture *enTexture = IMG_LoadTexture(mainRender, "Textures/enemy.png");
+	SDL_Rect enrect;
+	enrect.h = 32;
+	enrect.w = 32;
+	randSpawn(&enrect, bgrect, -1);
+
+	objects = newcontainer();
+	SDL_RendererFlip swordflip = SDL_FLIP_VERTICAL;
+
+	struct object player;
+	player.HP = 3;
+	player.LVL = 0;
+	player.DMG = 1;
+	player.moving = 0;
+	player.movingchek = 0;
+	player.playerchek = 1;
+	player.rectangle = prect;
+	player.texture = pTexture;
+
+	addtolist(&objects, player, 0);
+
+	struct object enemy;
+	enemy.HP = 2;
+	enemy.LVL = 0;
+	enemy.DMG = 1;
+	enemy.moving = 0;
+	enemy.movingchek = 0;
+	enemy.playerchek = 0;
+	enemy.rectangle = enrect;
+	enemy.texture = enTexture;
+
+	struct object stan;
+	stan.HP = -1;
+	stan.LVL = -1;
+	stan.DMG = 0;
+	stan.moving = -1;
+	stan.movingchek = 0;
+	stan.playerchek = 0;
+	stan.rectangle = strect;
+	stan.texture = stTexture;
+
+	addlast(&objects, enemy);
+
+	multiSpawn(mainRender, &objects, enemy, bgrect, 30);
+	multiSpawn(mainRender, &objects, stan, bgrect, 40);
+	printf("%i\n", element_count(&objects));
+
+	const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+
+	while(run == 1 && mainEvent->type != SDL_QUIT){
+		SDL_PollEvent(mainEvent);
+		SDL_PumpEvents();
+		SDL_SetRenderDrawColor(mainRender, 0, 0 ,0 ,0);
+		SDL_RenderClear(mainRender);
+		SDL_RenderCopy(mainRender, bgTexture, NULL, &bgrect);
+		if(keystate[SDL_SCANCODE_F] && attackrecharge == 0){
+			angle = 0;
+			objects = attack(getlist(&objects, 0)->data, &objects);
+			attackrecharge = 36;
+		}
+		if(attackrecharge > 0){
+			if(angle < 360){
+				angle += 10;
+				srect.x = getlist(&objects, 0)->data.rectangle.x + (getlist(&objects, 0)->data.rectangle.w / 10);
+				srect.y = getlist(&objects, 0)->data.rectangle.y + (getlist(&objects, 0)->data.rectangle.h / 2);
+				SDL_RenderCopyEx(mainRender, sTexture, NULL, &srect , angle, &swordCenter, swordflip);
+			}
+			attackrecharge--;
+		}
+		for(int i = 0; i < element_count(&objects); i++){
+			SDL_Rect currentRect = getlistdata(&objects, i).rectangle;
+			SDL_RenderCopy(mainRender, getlistdata(&objects, i).texture, NULL, &currentRect);
+		}
+		int *trafficbans = calculateTrafficBans(getlist(&objects, 0)->data, &objects);
+		getlist(&objects, 0)->data = playerMoving(getlist(&objects, 0)->data, trafficbans, keystate);
+		for(int i = 0; i < element_count(&objects); i++){
+			getlist(&objects, i)->data = entityMoving(getlist(&objects, i)->data, &objects, i);
+		}
+
+
+		SDL_RenderPresent(mainRender);
+		SDL_Delay(15);
+	}
+	SDL_DestroyRenderer(mainRender);
+	SDL_DestroyWindow(mainWindow);
+	SDL_Quit();
+
+	return 0;
+}
